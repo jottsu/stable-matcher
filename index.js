c97d78e0f3f9337319e93a03b6581468e986e277
Joichiro Hashikawa <j.hashikawa.tech@gmail.com>
2019-01-13 19:48:20 +0900

    divide mode of one-to-one and one-to-many

--- index.js
+++ index.js
@@ -1,3 +1,1 @@
-module.exports.createProposers = require('./lib/proposer').createProposers;
-module.exports.createAccepters = require('./lib/accepter').createAccepters;
 module.exports.Matcher = require('./lib/matcher');
